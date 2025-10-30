93221d7f4e33b6a48cb6e114ae5247a09662e318
Liyaz Nagoori <nagooriliyaz@gmail.com>
2025-10-30 20:59:07 +0530

    1.0

---
+++ workspace-sts/RestController-1/src/test/java/com/example/demo/RestController1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestController1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
